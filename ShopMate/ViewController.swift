b00247eded88663eec87835e75a50afaacdb878a
Hassaan Fayyaz Ahmed <hassaanfayyaz@gmail.com>
2019-05-15 23:16:34 +0500

    Xcode project created.

---
+++ ShopMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopMate
+//
+//  Created by Hassaan Fayyaz Ahmed on 5/15/19.
+//  Copyright © 2019 Hassaan Fayyaz Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
